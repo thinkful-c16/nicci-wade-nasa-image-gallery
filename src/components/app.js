3c601d803fb2dbad510339befb63f9d9a5f5f9d6
Owcollier <owcollier@gmail.com>
2017-12-13 11:57:32 -0500

    created top-level component app in components

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   index
+    // };
+  }
+
+  render() {
+    return (
+      console.log('hello world!')
+    );
+  }
+
+}
